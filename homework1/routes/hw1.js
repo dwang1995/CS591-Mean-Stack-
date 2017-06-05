c8e26b33976e978048e890563085eae16a1cdd47
dwang1995 <wangdayuan1995@gmail.com>
2017-06-05 10:04:32 -0400

    hw1

---
+++ homework1/routes/hw1.js
@@ -0,0 +1,35 @@
+/**
+ * Created by wangdayuan on 6/3/17.
+ */
+
+
+const express = require('express');
+const router = express.Router();
+const request = require("request");
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/:name', function(req,res,next){
+    let parName = req.params.name
+    let thelength = req.query.length
+    res.send({'string' : parName, 'length' : thelength})
+})
+
+router.post('/', function (req, res, next) {
+    res.json(req.body.test)
+    //res.send("hello")
+})
+
+/**
+router.post('/:name',function(req,res,next){
+    console.log(req.body.short)
+    let parName = req.params.name
+    let thelength = req.query.breed
+    res.send({'string' : parName, 'length' : thelength})
+    //res.send("hello")
+
+})*/
+
+module.exports = router;
